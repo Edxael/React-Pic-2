b01efd5ad6300aa9a2a7da0acb06dea7d5cb8af6
Edxael <zerphyr.r@gmail.com>
2018-06-04 11:41:57 -0600

    reset setTimeout()

--- src/AppCode/02-Page.jsx
+++ src/AppCode/02-Page.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
  
 export default class extends React.Component{
+    state = { uinput: '' }
     render(){
+        let myVar = setTimeout(() => { console.log("Update mande") }, 500)
         return(
             <div>
                 <h1>Page 2</h1>
+
+                <p>Testing reset SetTimeout - 1</p>
+
+                <input type="text" value={this.state.uinput} onChange={ (e) => {
+                    this.setState({ uinput: e.target.value })
+                    clearTimeout(myVar)
+                } } />
+
             </div>
         )
     }
